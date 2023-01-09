86f69a582103825041620177437d249335317fce
Naman Gera <namangera15@gmail.com>
2023-01-09 12:55:18 +0000

    WIP: start importing methods from tomopy

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,4 +1,5 @@
 import cupy as cp
+import h5py
 import numpy as np
 from cupy.testing import assert_allclose
 
@@ -6,9 +7,13 @@ from httomolib.prep.normalize import normalize_cupy
 from httomolib.prep.stripe import remove_stripe_based_sorting_cupy
 from httomolib.recon.rotation import find_center_vo_cupy
 
+from tomopy.prep.normalize import normalize
+from tomopy.prep.stripe import remove_stripe_based_sorting
+from tomopy.recon.rotation import find_center_vo
+
 
 def test_cpu_vs_gpu():
-    #: GPU pipeline tested on `tomo_standard`
+    #--- GPU pipeline tested on `tomo_standard` ---#
 
     in_file = 'tests/test_data/tomo_standard.npz'
     datafile = np.load(in_file)
@@ -21,7 +26,8 @@ def test_cpu_vs_gpu():
     darks = cp.asarray(host_darks)
 
     #: Normalize the data first
-    data_normalize_cupy = normalize_cupy(data, flats, darks, minus_log=True)
+    data_normalize_cupy = normalize_cupy(data, flats, darks)
+    assert data_normalize_cupy.shape == (180, 128, 160)
 
     #: Now do the stripes removal
     corrected_data = remove_stripe_based_sorting_cupy(data_normalize_cupy)
@@ -30,5 +36,17 @@ def test_cpu_vs_gpu():
 
     #: Set data collection angles as equally spaced between 0-180 degrees.
     cor = find_center_vo_cupy(corrected_data)
+    assert_allclose(cor, 79.5)
 
     #: Correct distortion
+
+
+    #--- CPU pipeline tested on `tomo_standard` ---#
+
+    tomopy_data = normalize(host_data, host_flats, host_darks)
+    assert tomopy_data.shape == (180, 128, 160)
+
+    tomopy_corrected_data = remove_stripe_based_sorting(tomopy_data)
+
+    tomopy_cor = find_center_vo(tomopy_corrected_data)
+    assert_allclose(tomopy_cor, cor)
