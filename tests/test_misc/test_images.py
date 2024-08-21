2d6c535a741b823d52415e2f57c9118be1cc0fef
dkazanc <dkazanc@hotmail.com>
2024-08-21 15:45:06 +0100

    this adds watermark functionality to save_to_images

--- tests/test_misc/test_images.py
+++ tests/test_misc/test_images.py
@@ -12,7 +12,31 @@ import time
 @pytest.mark.parametrize("bits", [8, 16, 32])
 def test_save_to_images(host_data: np.ndarray, tmp_path: pathlib.Path, bits: int):
     # --- Test for bits=8
-    save_to_images(host_data[:, :10, :].astype(np.float32), tmp_path / "save_to_images", bits=bits)
+    save_to_images(
+        host_data[:, :10, :].astype(np.float32), tmp_path / "save_to_images", bits=bits
+    )
+
+    folder = tmp_path / "save_to_images" / "images" / f"images{bits}bit_tif"
+    assert folder.exists()
+    files = list(folder.glob("*"))
+
+    assert len(files) == 10
+    for f in files:
+        assert f.name[-3:] == "tif"
+        assert Image.open(f).size == (host_data.shape[2], host_data.shape[0])
+
+
+@pytest.mark.parametrize("bits", [8, 16, 32])
+def test_save_to_images_watermark(
+    host_data: np.ndarray, tmp_path: pathlib.Path, bits: int
+):
+    # --- Test for bits=8
+    save_to_images(
+        host_data[:, :10, :].astype(np.float32),
+        tmp_path / "save_to_images",
+        bits=bits,
+        watermark_txt="text",
+    )
 
     folder = tmp_path / "save_to_images" / "images" / f"images{bits}bit_tif"
     assert folder.exists()
@@ -82,13 +106,15 @@ def test_save_to_images_other_bits_default_to_32(
 def test_glob_stats_percentile_computation(
     host_data: np.ndarray, tmp_path: pathlib.Path, mocker: MockerFixture
 ):
-    save_single_mock = mocker.patch("httomolib.misc.images._rescale_2d", return_value=host_data[:, 1, :])
+    save_single_mock = mocker.patch(
+        "httomolib.misc.images._rescale_2d", return_value=host_data[:, 1, :]
+    )
     save_to_images(
         np.squeeze(host_data[:, 1, :]).astype(np.float32),
         tmp_path / "save_to_images",
         bits=8,
         file_format="tif",
-        glob_stats=(20., 60., 40., 123),
+        glob_stats=(20.0, 60.0, 40.0, 123),
         perc_range_min=10.0,
         perc_range_max=90.0,
     )
@@ -96,9 +122,7 @@ def test_glob_stats_percentile_computation(
     min_percentile = 10.0 * 40.0 / 100.0 + 20
     max_percentile = 90.0 * 40.0 / 100.0 + 20
 
-    save_single_mock.assert_called_once_with(
-        ANY, 8, min_percentile, max_percentile
-    )
+    save_single_mock.assert_called_once_with(ANY, 8, min_percentile, max_percentile)
 
 
 def test_integer_input_does_not_rescale(
@@ -110,7 +134,7 @@ def test_integer_input_does_not_rescale(
         tmp_path / "save_to_images",
         bits=8,
         file_format="tif",
-        glob_stats=(20., 60., 40., 123),
+        glob_stats=(20.0, 60.0, 40.0, 123),
         perc_range_min=10.0,
         perc_range_max=90.0,
     )
@@ -119,24 +143,28 @@ def test_integer_input_does_not_rescale(
     folder = tmp_path / "save_to_images" / "images" / "images8bit_tif"
     assert folder.exists()
 
+
 @pytest.mark.perf
 def test_save_to_images_performance(tmp_path: pathlib.Path):
-    data = np.random.randint(low=0, high=255, size=(160,1800,160), dtype=np.uint8)
-    
+    data = np.random.randint(low=0, high=255, size=(160, 1800, 160), dtype=np.uint8)
+
     # uncomment and adapt to save / test different locations
     # tmp_path = pathlib.Path(
     #     '/mnt/gpfs03/scratch/data/imaging/tomography/tmp/image-saver-perf-test'
     # )
-    
-    start = time.perf_counter_ns()
-    save_to_images(data, tmp_path / "save_to_images1", bits=8, axis=1, asynchronous=False)
-    end = time.perf_counter_ns()
-    duration_ms_old = (end-start)*1e-6
-    
-    start = time.perf_counter_ns()
-    save_to_images(data, tmp_path / "save_to_images2", bits=8, axis=1, asynchronous=True)
-    end = time.perf_counter_ns()
-    duration_ms = (end-start)*1e-6
-    
-    assert duration_ms_old == duration_ms
 
+    start = time.perf_counter_ns()
+    save_to_images(
+        data, tmp_path / "save_to_images1", bits=8, axis=1, asynchronous=False
+    )
+    end = time.perf_counter_ns()
+    duration_ms_old = (end - start) * 1e-6
+
+    start = time.perf_counter_ns()
+    save_to_images(
+        data, tmp_path / "save_to_images2", bits=8, axis=1, asynchronous=True
+    )
+    end = time.perf_counter_ns()
+    duration_ms = (end - start) * 1e-6
+
+    assert duration_ms_old == duration_ms
